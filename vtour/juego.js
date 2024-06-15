184895b12b7d413f1cf004b66990833a5c26f34a
espisepi <sepinaco@gmail.com>
2024-06-15 23:56:39 +0200

    improve juego.js

--- vtour/juego.js
+++ vtour/juego.js
@@ -1,18 +1,51 @@
+
+var PUNTUACION_INICIAL = 10;
+
 var juego = {
-    puntaje: 10,
+
+    puntaje: PUNTUACION_INICIAL,
   
     itemClick: function(itemName) {
       switch(itemName) {
         case 'item1':
           this.puntaje--;
-          alert("¡Te quedan " +  this.puntaje + " puntos!" );
+        //   alert("¡Te quedan " +  this.puntaje + " puntos!" );
           break;
         case 'item2':
           this.puntaje--;
-          alert("¡Te quedan " +  this.puntaje + " puntos!" );
+        //   alert("¡Te quedan " +  this.puntaje + " puntos!" );
           break;
         // Maneja otros ítems aquí
       }
+      // render ui
+      this.render();
+    },
+
+    render: function() {
+        this.showUI();
+        this.updateContador();
+    },
+
+    showUI: function() {
+        var divContadorElement = document.querySelector('.juego-contador');
+        divContadorElement.style.display = 'block';
+    },
+
+    hiddenUI: function() {
+        var divContadorElement = document.querySelector('.juego-contador');
+        divContadorElement.style.display = 'none';
+    },
+
+    updateContador: function() {
+        var h1ContadorElement = document.querySelector('.juego-contador h1');
+        h1ContadorElement.textContent = this.puntaje;
+    },
+
+    restore: function() {
+        this.puntaje = PUNTUACION_INICIAL;
+        this.render();
     }
+
+    
   };
   
