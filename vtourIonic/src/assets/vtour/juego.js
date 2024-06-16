b29e0bb168f403dce6fd8c7e10058fbff01cff11
espisepi <sepinaco@gmail.com>
2024-06-16 05:40:33 +0200

    added vtour krpano

---
+++ vtourIonic/src/assets/vtour/juego.js
@@ -0,0 +1,77 @@
+
+var PUNTUACION_INICIAL = 10;
+
+var juego = {
+
+    puntaje: PUNTUACION_INICIAL,
+  
+    itemClick: function(itemName) {
+        // console.log({krpano:window.krpano})
+      switch(itemName) {
+        case 'item1':
+          this.puntaje--;
+          break;
+        case 'item2':
+          this.puntaje--;
+          break;
+      }
+      // render ui
+      this.render();
+    },
+
+    render: function() {
+        this.updateHotspotText();
+        // this.showUIContador();
+        // this.updateContador();
+    },
+
+    showUIContador: function() {
+        // var divContadorElement = document.querySelector('.juego-contador');
+        // divContadorElement.style.display = 'block';
+    },
+
+    hiddenUIContador: function() {
+        // var divContadorElement = document.querySelector('.juego-contador');
+        // divContadorElement.style.display = 'none';
+    },
+
+    updateContador: function() {
+        // var h1ContadorElement = document.querySelector('.juego-contador h1');
+        // h1ContadorElement.textContent = this.puntaje;
+    },
+
+    restartGame: function() {
+
+        // restore contador
+        this,hiddenUIContador();
+        this.puntaje = PUNTUACION_INICIAL;
+        this.updateContador();
+
+        // restore scene krpano
+
+    },
+
+    showUIGameover: function() {
+
+    },
+    hiddenUIGameover: function() {
+
+    },
+
+    // krpano ======================
+
+    updateHotspotText: function() {
+        var hotspotName = "puntuacion";
+        var hotspot = krpano.get("hotspot[" + hotspotName + "]");
+        if (hotspot) {
+        //   hotspot.html = '<div style="background: rgba(0,0,0,0.5); padding: 5px; border-radius: 5px; color: white;">' + this.puntaje + '</div>';
+        hotspot.html = '<div class="hotspot-puntuacion-container"><div class="hotspot-puntuacion">' + this.puntaje + '</div></div>';
+        }
+    },
+
+      // Example of changing the text of the hotspot
+    //   changeHotspotText("text_hotspot", "New Text Here");
+
+    
+  };
+  
---
+++ vtourIonic/src/assets/vtour/videoPerformance.js
@@ -0,0 +1,69 @@
+
+
+var videoPerformance = {
+
+  krpano: null,
+
+  checkVideoVisibility: function() {
+    this.krpano = window.krpano;
+    if(!krpano) {
+      return;
+    }
+    this._checkVideoVisibility();
+  },
+
+  _checkVideoVisibility: function() {
+
+    var krpano = this.krpano;
+
+    var hlookat = krpano.get('view.hlookat');
+
+    var vlookat = krpano.get('view.vlookat');
+    var fov = krpano.get('view.fov') / 2;
+
+    var hotspots = krpano.get('hotspot').getArray();
+
+    var self = this;
+
+    hotspots.forEach(function(hotspot) {
+
+      var exit = false;
+
+      // Videos excepciones que no queremos que se paren
+      if(hotspot.name == "portal_linea2") {
+          exit = true;
+      }
+
+      if(!exit && hotspot.url === "plugins/videoplayer.js") {
+        var videoAth = hotspot.ath;
+        var videoAtv = hotspot.atv;
+
+        var videoVisible = self.isInFieldOfView(hlookat, vlookat, fov, videoAth, videoAtv);
+
+        // console.log({videoVisible});
+        self.controlVideoPlayback(hotspot.name, videoVisible);
+      }
+      
+    });
+
+  },
+
+  isInFieldOfView: function(hlookat, vlookat, fov, ath, atv) {
+    var hDiff = Math.abs(hlookat - ath);
+    var vDiff = Math.abs(vlookat - atv);
+
+    return hDiff <= fov && vDiff <= fov;
+  },
+
+  controlVideoPlayback: function(hotspotName, shouldPlay) {
+    // var krpano = document.getElementById('krpanoSWFObject').get('krpano');
+    var krpano = this.krpano;
+    if (shouldPlay) {
+      krpano.call('hotspot[' + hotspotName + '].play();');
+    } else {
+      krpano.call('hotspot[' + hotspotName + '].pause();');
+    }
+  }
+
+};
+  
