1fbd2a621b2d490b8bb3550623914c5f4c6af6ba
espisepi <sepinaco@gmail.com>
2024-06-17 13:31:42 +0200

    fixed threejsm run twice

--- vtour/plugin-threejs/three-jsm-master/src/main.js
+++ vtour/plugin-threejs/three-jsm-master/src/main.js
@@ -1,7 +1,7 @@
 import App from './App.js';
 
 const app = new App();
-app.init();
+// app.init(); // El metodo init() se llama ahora en el contructor de App
 
 // save app in global variable
 window.threejsm = app;
