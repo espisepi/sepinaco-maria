db1981305286aa3b2dd31de005f7ac8b6bbccedc
espisepi <sepinaco@gmail.com>
2024-06-19 22:34:52 +0200

    added linter

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// eslint.config.js
+export default [
+    {
+      ignores: ['node_modules/**'],
+      files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+      rules: {
+        // tus reglas de ESLint aquí
+      }
+    }
+  ];
